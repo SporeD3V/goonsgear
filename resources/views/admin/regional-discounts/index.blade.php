ca0f2a8b71174ef7252544d2160a132b43d98c22
Matei Panchios <mateipanchios@gmail.com>
2026-04-07 22:23:53 +0300

    Fix corrupted regional-discounts index view (leftover old Blade content)

--- resources/views/admin/regional-discounts/index.blade.php
+++ resources/views/admin/regional-discounts/index.blade.php
@@ -3,10 +3,3 @@
 @section('content')
     <livewire:admin.regional-discount-manager />
 @endsection
-                @endforelse
-            </tbody>
-        </table>
-    </div>
-
-    <div class="mt-4">{{ $discounts->links() }}</div>
-@endsection
